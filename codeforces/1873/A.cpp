127b68431cd1e1c8ed4a34a3f224ce15fc01a10c
Oppenbhaimer <oppenbhaimer@gmail.com>
2023-09-21 20:06:00 +0530

    Add solution for problem `A - Short Sort` Link: https://codeforces.com/contest/1873/problem/A Tags: brute force, implementation Ref: https://codeforces.com/contest/1873/submission/224312625

---
+++ codeforces/1873/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD2 998244353
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+#define per(i, a, b) for (auto i = (a); i >= (b); i--)
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+
+void solve() {
+
+    string s;
+    cin >> s;
+    if (s == "bca" || s == "cab") cout << "NO\n";
+    else cout << "YES\n";
+
+}
+
+int main(int argc, char** argv) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        solve();
+    }
+    return 0;
+}
+
