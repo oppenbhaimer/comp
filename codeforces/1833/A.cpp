35756e2279ba9da7362619700f96c13d18690a98
Oppenbhaimer <oppenbhaimer@gmail.com>
2023-11-16 10:43:00 +0530

    Add solution for problem `A - Musical Puzzle` Link: https://codeforces.com/contest/1833/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1833/submission/232924913

---
+++ codeforces/1833/A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD2 998244353
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+
+void solve() {
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    unordered_set<string> S;
+    rep(i, 0, n-1) {
+        S.insert(s.substr(i, 2));
+    }
+
+    cout << S.size() << endl;
+}
+
+int main(int argc, char** argv) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t-- > 0) {
+        solve();
+    }
+    return 0;
+}
+
