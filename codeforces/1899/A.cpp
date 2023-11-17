26ddc9e93fff6dafae2353c1f7bc43d211d1646c
Oppenbhaimer <oppenbhaimer@gmail.com>
2023-11-17 20:06:00 +0530

    Add solution for problem `A - Game with Integers` Link: https://codeforces.com/contest/1899/problem/A Tags: games, math, number theory Ref: https://codeforces.com/contest/1899/submission/233098188

---
+++ codeforces/1899/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD2 998244353
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+
+void solve() {
+
+    int n;
+    cin >> n;
+
+    if (n%3 == 0) cout << "Second" << endl;
+    else cout << "First" << endl;
+}
+
+int main(int argc, char** argv) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t-- > 0) {
+        solve();
+    }
+    return 0;
+}
+
