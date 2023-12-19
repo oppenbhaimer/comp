f15475e41a4fb318b473d7d07f472080e53eceb9
Oppenbhaimer <oppenbhaimer@gmail.com>
2023-12-19 20:11:00 +0530

    Add solution for problem `B - Preparing for the Contest` Link: https://codeforces.com/contest/1914/problem/B Tags: constructive algorithms, math Ref: https://codeforces.com/contest/1914/submission/237949811

---
+++ codeforces/1914/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD2 998244353
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+#define per(i, a, b) for (auto i = (a); i >= (b); i--)
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+
+void solve() {
+
+    int n, k;
+    cin >> n >> k;
+
+
+    rep(i, 1, n+1) {
+        if (i <= k) cout << i << " ";
+        else cout << (n+k+1)-i << " ";
+    }
+    cout << endl;
+}
+
+int main(int argc, char** argv) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t-- > 0) {
+        solve();
+    }
+    return 0;
+}
+
